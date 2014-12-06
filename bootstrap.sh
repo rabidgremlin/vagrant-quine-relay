587e4a8e2322d14a3ee5b2980bb6323eb367e925
Javier Candeira <javier@candeira.com>
2014-12-06 14:44:21 +1100

    add support for D, the 82nd language

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,10 +16,10 @@ export DEBIAN_FRONTEND=noninteractive
 # get all the compilers/tools
 apt-get install -q -y afnix algol68g aplus-fsf asymptote \
   ats-lang-anairiats bash bc bf boo bsdgames cduce clisp clojure1.4 \
-  cmake coffeescript f2c fp-compiler g++ gauche gawk gcc gforth gfortran \
-  ghc ghostscript gnat gnu-smalltalk gobjc golang groff groovy icont \
-  iconx intercal iverilog jasmin-sable libgd2-xpm-dev libpng12-dev llvm \
-  lua5.2 make maxima mlton mono-devel mono-mcs mono-vbnc nasm neko \
+  cmake coffeescript f2c fp-compiler g++ gauche gawk gcc gdc gforth \
+  gfortran ghc ghostscript gnat gnu-smalltalk gobjc golang groff groovy \
+  icont iconx intercal iverilog jasmin-sable libgd2-xpm-dev libpng12-dev \
+  llvm lua5.2 make maxima mlton mono-devel mono-mcs mono-vbnc nasm neko \
   nickle ocaml octave open-cobol openjdk-6-jdk pari-gp parrot perl \
   php5-cli pike7.8 python r-base ratfor regina-rexx rhino ruby2.0 scala \
   scilab slsh spl-core swi-prolog tcl ucblogo valac xsltproc yorick zoem \
@@ -39,8 +39,8 @@ cd /vagrant/qr
 git clone https://github.com/mame/quine-relay .
 
 # pin this version of vagrant-qr to a specific commit of quine-relay:
-# the one for language #81 Chef, to avoid dependency failure
-git checkout 406b154716f1b0775925be39a1d9b34c9ce7ddd7
+# the one for language #82 D, to avoid dependency failure
+git checkout 468074a323a285eafb15e6553feccaa45197ebad
 
 # make non-ubuntu-packaged language dependencies
 cd vendor && make && cd ..
