feca4fe6e8b0e9f8571d18effb397266f82a89a1
Javier Candeira <javier@candeira.com>
2014-12-10 10:08:16 +1100

    add support for Emacs Lisp, the 85th language
      - also suppress some noisy output from git
      - and the 84th language was eC, not eD dammit

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,13 +16,14 @@ export DEBIAN_FRONTEND=noninteractive
 # get all the compilers/tools
 apt-get install -q -y afnix algol68g aplus-fsf asymptote \
   ats-lang-anairiats bash bc bf boo bsdgames cduce clisp clojure1.4 \
-  cmake coffeescript dc ecere-sdk f2c fp-compiler g++ gauche gawk gcc gdc \
-  gforth gfortran ghc ghostscript gnat gnu-smalltalk gobjc golang groff \
-  groovy icont iconx intercal iverilog jasmin-sable libgd2-xpm-dev \
-  libpng12-dev llvm lua5.2 make maxima mlton mono-devel mono-mcs mono-vbnc \
-  nasm neko nickle ocaml octave open-cobol openjdk-6-jdk pari-gp parrot perl \
-  php5-cli pike7.8 python r-base ratfor regina-rexx rhino ruby2.0 scala \
-  scilab slsh spl-core swi-prolog tcl ucblogo valac xsltproc yorick zoem \
+  cmake coffeescript dc ecere-sdk emacs23 f2c fp-compiler g++ gauche \
+  gawk gcc gdc gforth gfortran ghc ghostscript gnat gnu-smalltalk gobjc \
+  golang groff groovy icont iconx intercal iverilog jasmin-sable \
+  libgd2-xpm-dev libpng12-dev llvm lua5.2 make maxima mlton mono-devel \
+  mono-mcs mono-vbnc nasm neko nickle ocaml octave open-cobol \
+  openjdk-6-jdk pari-gp parrot perl php5-cli pike7.8 python r-base \
+  ratfor regina-rexx rhino ruby2.0 scala scilab slsh spl-core swi-prolog \
+  tcl ucblogo valac xsltproc yorick zoem
   git
 
 # otherwise step ppt -> Prolog will fail with a:
@@ -40,7 +41,7 @@ git clone https://github.com/mame/quine-relay .
 
 # pin this version of vagrant-qr to language #84 eC
 # this is a specific commit to avoid dependency failure
-git checkout e64a4d9d1c782da0d39397152ccbea219000a3ab
+git checkout 12aaa5b785e0944f32fb03b5a484578582b24b16 >> /dev/null
 
 # make non-ubuntu-packaged language dependencies
 cd vendor && make && cd ..
@@ -50,3 +51,4 @@ make
 
 # leave qr quine-relay repo in a good state
 cd /vagrant/qr && git reset --hard master >> /dev/null
+git checkout master >> /dev/null
